2157ebebea808890491fb0d7f6f5ad10a0e971ca
mohammad2001ah <mah.2001.ah@gmail.com>
2025-03-09 14:45:52 +0300

    1

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,10 @@ import Title from './componenets/Title';
 import Button from './componenets/Button';
 import Input from './componenets/Input';
 import TodoList from './componenets/TodoList';
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import { saveToLocalStorage,getFromLocalStorage } from './utils/storage';
 import React,{useState,useEffect} from 'react';
+import MediaCard from './componenets/cards';
 
 function App() {
   //state for input
@@ -25,13 +26,19 @@ function App() {
     setTodos ([...todos,inputValue]);
   }
   return (
-    <div className="App">
-      <NavBar/>
-      <Title text="This is react to do app"/>
-      <Input value={inputValue} onChange={setInputValue}/>
-      <Button text="add to do" onClick={handelAdd}/>
-      <TodoList todos={todos}/>
-    </div>
+    // <div className="App">
+    //   <NavBar/>
+    //   <Title text="This is react to do app"/>
+    //   <Input value={inputValue} onChange={setInputValue}/>
+    //   <Button text="add to do" onClick={handelAdd}/>
+    //   <TodoList todos={todos}/>
+    // </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path ='/navBar' element={<NavBar/>}/>
+        <Route path='/' element={<MediaCard/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
