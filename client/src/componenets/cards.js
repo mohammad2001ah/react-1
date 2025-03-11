665d96448be4db1330cfcd8a60ef95086a4d35c7
mohammad2001ah <mah.2001.ah@gmail.com>
2025-03-11 14:47:39 +0300

    Update cards.js

--- client/src/componenets/cards.js
+++ client/src/componenets/cards.js
@@ -1,32 +1,61 @@
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import { Grid, Card, CardActions, CardContent, CardMedia, Button, Typography } from '@mui/material';
 
 export default function MediaCard() {
+  const cardsData = [
+    {
+      title: "Card 1",
+      description: "Card 1 Description",
+      image: "/image/download.jpeg",
+    },
+    {
+      title: "Card 2",
+      description: "Card 2 Description",
+      image: "/image/download.jpeg",
+    },
+    {
+      title: "Card 3",
+      description: "Card 3 Description",
+      image: "/image/download.jpeg",
+    },
+    {
+      title: "Card 4",
+      description: "Card 3 Description",
+      image: "/image/download.jpeg",
+    },
+    {
+      title: "Card 5",
+      description: "Card 3 Description",
+      image: "/image/download.jpeg",
+    },
+    {
+      title: "Card 6",
+      description: "Card 3 Description",
+      image: "/image/download.jpeg",
+    },
+  ];
+
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image="public/image/download.jpeg"
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography>
-        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
-    </Card>
+    <Grid container spacing={2} justifyContent="center">
+      {cardsData.map((card, index) => (
+        <Grid item xs={12} sm={6} md={4} key={index}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia sx={{ height: 140 }} image={card.image} title={card.title} />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {card.title}
+              </Typography>
+              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                {card.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      ))}
+    </Grid>
   );
 }
