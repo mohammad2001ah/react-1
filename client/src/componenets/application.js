9f36fd4cf6944e72df739d961caa513dfc623110
mohammad2001ah <mah.2001.ah@gmail.com>
2025-03-22 02:02:56 +0300

    Update application.js

--- client/src/componenets/application.js
+++ client/src/componenets/application.js
@@ -1,3 +1,4 @@
+import { Update } from "@mui/icons-material";
 import { useState } from "react";
 // export default function Counter(){
 //   const[count,setCount]=useState(0);
@@ -20,22 +21,44 @@ import { useState } from "react";
 export default function Counter(){
   const[text,setText]=useState("");
   const[todo,setTodo]=useState([]);
+
   function addTask(event){
     event.preventDefault();
     if(text.trim()===""){
       alert("enter yor task");
       return;
     }
+    const isEditing = todo.find(item => item.isEditing === true);
+    if(isEditing){
+      setTodo(todo.map(item =>
+        item.id === isEditing.id ? { ...item, text: text, isEditing: false } : item
+      ));
+      setText("");
+      return;
+    }
     const newTask={
       id: Date.now(),
-      text:text.trim()
+      text: text.trim(),
+      isEditing: false
+    }
+    setTodo([...todo, newTask]);
+    setText("");
   }
-    setTodo([...todo,newTask])
-    setText(" ");
-  }
+
   function deleteTask(id){
-    setTodo(todo.filter((item=>item.id !==id)))
+    setTodo(todo.filter(item => item.id !== id));
   }
+
+  function editData(id){
+    const edit = todo.find(item => item.id === id);
+    if(edit){
+      setText(edit.text);
+      setTodo(todo.map(item =>
+        item.id === id ? { ...item, isEditing: true } : { ...item, isEditing: false }
+      ));
+    }
+  }
+
   return(
     <>
     <h1>todo list??</h1>
@@ -47,6 +70,7 @@ export default function Counter(){
       {todo.map(item =>
         <li key={item.id}>{item.text}
         <button onClick={() => deleteTask(item.id)}>Delete</button>
+        <button onClick={() => editData(item.id)}>edit</button>
         </li>
       )}
     </ul>
